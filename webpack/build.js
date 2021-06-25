9ce65579a8d36ef1d7101df873793d4858f4161f
Liam Li <398389303@qq.com>
2021-06-25 23:30:02 +0800

    add building

--- webpack/build.js
+++ webpack/build.js
@@ -1,17 +1,104 @@
-// const fs = require('fs')
-const Webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
-const webpackConfig = require('./webpack.config')
-const devServerConfig = require('./webpack-dev-server.config')
+//再怎么敲命令都不允许覆盖😈
+process.env.BABEL_ENV = 'production'
+process.env.NODE_ENV = 'production'
 
-const compiler = Webpack(webpackConfig)
-
-const devServerOptions = Object.assign({}, devServerConfig, {
-  open: true,
+process.on('unhandledRejection', err => {
+  throw err
 })
+require('./env')
+const chalk = require('chalk')
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const configFactory = require('./webpack.config')
+const paths = require('./paths')
+const formatWebpackMessages = require('./utils/formatWebpackMessages')
 
-const server = new WebpackDevServer(devServerOptions, compiler)
+//const useYarn = fs.existsSync(path.resolve(paths.appBase, 'yarn.lock'))
 
-// server.listen(devServerOptions.port, '127.0.0.1', () => {
-//   console.log('Starting server on http://localhost:' + devServerOptions.port)
-// })
+// These sizes are pretty large. We'll warn for bundles exceeding them.
+const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024
+const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024
+
+const isInteractive = process.stdout.isTTY
+
+const argv = process.argv.slice(2)
+const writeStatsJson = argv.indexOf('--stats') !== -1
+
+// Generate configuration
+const config = configFactory('production')
+
+function build(previousFileSizes) {
+  console.log('Creating an optimized production build...')
+
+  const compiler = webpack(config)
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      let messages
+      if (err) {
+        if (!err.message) {
+          return reject(err)
+        }
+
+        let errMessage = err.message
+
+        // Add additional information for postcss errors
+        if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
+          errMessage += '\nCompileError: Begins at CSS selector ' + err['postcssNode'].selector
+        }
+
+        messages = formatWebpackMessages({
+          errors: [errMessage],
+          warnings: [],
+        })
+      } else {
+        messages = formatWebpackMessages(stats.toJson({ all: false, warnings: true, errors: true }))
+      }
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1
+        }
+        return reject(new Error(messages.errors.join('\n\n')))
+      }
+      if (
+        process.env.CI &&
+        (typeof process.env.CI !== 'string' || process.env.CI.toLowerCase() !== 'false') &&
+        messages.warnings.length
+      ) {
+        console.log(
+          chalk.yellow(
+            '\nTreating warnings as errors because process.env.CI = true.\n' +
+              'Most CI servers set it automatically.\n',
+          ),
+        )
+        return reject(new Error(messages.warnings.join('\n\n')))
+      }
+
+      const resolveArgs = {
+        stats,
+        previousFileSizes,
+        warnings: messages.warnings,
+      }
+
+      if (writeStatsJson) {
+        return bfj
+          .write(paths.appBuild + '/bundle-stats.json', stats.toJson())
+          .then(() => resolve(resolveArgs))
+          .catch(error => reject(new Error(error)))
+      }
+
+      return resolve(resolveArgs)
+    })
+  })
+}
+
+try {
+  build(10000)
+} catch (err) {
+  if (err && err.message) {
+    console.log(err.message)
+  }
+  process.exit(1)
+}
